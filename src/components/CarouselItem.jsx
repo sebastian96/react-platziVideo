bef1528946f88a3dac3e86c7efdf4e8d9a98af19
Sebastian Miranda <arqcompu4@gmail.com>
2020-01-22 21:06:29 -0500

    update

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import endGame from '../assets/static/poster/end_game.jpg';
 import play from '../assets/static/play.png';
 import plus from '../assets/static/plus.png';
 import '../assets/styles/components/CarouselItem.scss';
 
-const CarouselItem = () => (
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     <div className="carousel__item">
-        <img className="carousel__item--poster" src={endGame} alt="Avengers end game" />
+        <img className="carousel__item--poster" src={cover} alt="Avengers end game" />
         <div className="carousel__item--details">
             <div className="icons">
                 <img src={play} alt="Ver pelicula" />
                 <img src={plus} alt="Ver detalles de pelicula"/>
             </div>
-            <p className="title">Avengers end game</p>
-            <p className="subTitle">2019 +14 190 minutos</p>
+            <p className="title">{title}</p>
+            <p className="subTitle">
+                {`${year} ${contentRating} ${duration}`}
+            </p>
         </div>
     </div>
 );
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,41 +1,56 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
+
 import '../assets/styles/App.scss';
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <main className="main">
-            <Search />
-            <Categories title="Lo mas visto">
-                <Carousel>
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
-                </Carousel>
-            </Categories>
+const API = 'http://localhost:3000/initalState';
 
-            <Categories title="Mi lista">
-                <Carousel>
-                    <CarouselItem />
-                    <CarouselItem />
-                </Carousel>
-            </Categories>
+const App = () => {
+    
+    const initialState = useInitialState(API);
 
-            <Categories title="Lo mas nuevo">
-                <Carousel>
-                    <CarouselItem />
-                </Carousel>
-            </Categories>
-        </main>
-        <Footer />
-    </div>
-);
+    return initialState.length === 0 ? <h1>Loading...</h1>: (
+        <div className="App">
+            <Header />
+            <main className="main">
+                <Search />
+
+                {initialState.mylist.length > 0 &&
+                    <Categories title="Mi lista">
+                        <Carousel>
+                            {initialState.mylist.map(item => 
+                                <CarouselItem key={item.id} {...item} />
+                            )}
+                        </Carousel>
+                    </Categories>
+                }
+
+                <Categories title="Lo mas visto">
+                    <Carousel>
+                        {initialState.trends.map(item =>
+                            <CarouselItem key={item.id} {...item} />
+                        )}
+                    </Carousel>
+                </Categories>
+
+
+                <Categories title="Lo mas nuevo">
+                    <Carousel>
+                        {initialState.originals.map(item => 
+                            <CarouselItem key={item.id} {...item} />
+                        )}
+                    </Carousel>
+                </Categories>
+            </main>
+            <Footer />
+        </div>
+    );
+};
 
 export default App;
