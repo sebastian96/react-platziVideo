d11dbbd3f1a6f5bcde7fa4eeebe3ca65a48a3bbe
sebastian miranda <arqcompu4@gmail.com>
2020-03-28 19:49:16 -0500

    se hace validacion e footer y se elimina paquete de npm

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,16 @@ import '../assets/styles/components/Footer.scss';
 
 const Footer = props => {
     const { location } = props;
+    let className;
+    
+    if(location.key || location.pathname === '/') {
+        className = 'footer'
+    } else {
+        className = 'footer notFound';
+    }
+    
     return(
-        <footer className={`${location.key ? 'footer' : 'footer notFound'}`}>
+        <footer className={`${className}`}>
             <a href="">Términos de uso</a>
             <a href="">Declaración de privacidad</a>
             <a href="">Centro de ayuda</a>
